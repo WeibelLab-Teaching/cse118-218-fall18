16df00d2f8ad850e681343b681ad196b11c7d65e
Danilo Gasques <danilod100@gmail.com>
2018-11-05 15:10:52 -0800

    Adding code for the lab

---
+++ lab4-kinect-hololens/KinectOverUDP/Assets/Scripts/MessageManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class HeadPose
+{
+    public int id;
+    public float headx, heady, headz;
+}
+
+public class MessageManager : MonoBehaviour {
+
+    public Transform cube;
+
+    public void OnJSONMessage(string msg)
+    {
+       
+        HeadPose head = JsonUtility.FromJson<HeadPose>(msg);
+        //Debug.Log("Head " +  head.id + " - " + head.headx + "  " + head.heady + "  " + head.headz);
+        if (head != null && head.headx > 0)
+        {
+            Vector3 newPosition = new Vector3(head.headx, head.heady, head.headz);
+            cube.localPosition = newPosition;
+        }
+ 
+
+    }
+
+}
